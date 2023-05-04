d0f4e7828f9505e5c1623e14daf577fb9481e452
yigitopan <yigitopan@hotmail.com>
2023-05-04 19:55:02 +0300

    invalid foreign key constraint test - pass

--- api/test/roleprivilege-test.js
+++ api/test/roleprivilege-test.js
@@ -11,7 +11,7 @@ describe('/roleprivileges', () => {
 		agent
 			.post('/user/login')
 			//.send({ tc: '3041041', password: '123456' }) User without privilege returns 401 - test fails.
-			.send({ tc: '0', password: '123456' }) // ADMIN Credentials.
+			.send({ tc: '0', password: '123456' }) 
 			.then(function (res) {
 				expect(res).to.have.status(200);
 				done();        
@@ -30,9 +30,20 @@ describe('/roleprivileges', () => {
 	it('/add should add dummy rolePrivilege into db', (done) => {
 		agent
 			.post('/roleprivilege/add')
-			.send({ role_id: 2, privilege_id: 12 }) // ADMIN Credentials.
+			.send({ role_id: 2, privilege_id: 12 }) 
 			.then(function (res) {
 				expect(res).to.have.status(201);
+				done();        
+			}).catch((err) => done(err));
+	});
+
+	it('/add should FAIL, because of invalid foreign key.', (done) => {
+		agent
+			.post('/roleprivilege/add')
+			.send({ role_id: 31, privilege_id: 12 }) 
+			.then(function (res) {
+				//violates foreign key constraint
+				expect(res.text).to.contain('violates foreign key constraint');
 				done();        
 			}).catch((err) => done(err));
 	});
